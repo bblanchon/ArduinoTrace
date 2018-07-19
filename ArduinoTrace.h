c56cc8fdf46a235744c7ab2f1bd6b2be75389be8
Benoit Blanchon <github@benoitblanchon.fr>
2018-07-19 12:06:36 +0200

    Disable PROGMEM on ESP8266

--- ArduinoTrace.h
+++ ArduinoTrace.h
@@ -19,6 +19,15 @@
 
 #include <Arduino.h>
 
+#ifndef ARDUINOTRACE_ENABLE_PROGMEM
+#if ARDUINO_ARCH_ESP8266
+// avoid error 'XXX causes a section type conflict with YYY'
+#define ARDUINOTRACE_ENABLE_PROGMEM 0
+#else
+#define ARDUINOTRACE_ENABLE_PROGMEM 1
+#endif
+#endif
+
 namespace ArduinoTrace {
 struct Initializer {
   Initializer(int bauds) {
@@ -29,35 +38,49 @@ struct Initializer {
 };
 
 struct Tracer {
-  Tracer(const char *location, const char *function) {
-    Serial.print(reinterpret_cast<const __FlashStringHelper *>(location));
+#if ARDUINOTRACE_ENABLE_PROGMEM
+  typedef const __FlashStringHelper *location_type;
+#else
+  typedef const char *location_type;
+#endif
+
+  Tracer(location_type location, const char *function) {
+    Serial.print(location);
     Serial.println(function);
     Serial.flush();
   }
 };
 } // namespace ArduinoTrace
 
-#define TRACE_STR(X) #X
-#define TRACE_STRINGIFY(X) TRACE_STR(X)
-#define TRACE_CONCAT(X, Y) X##Y
-#define TRACE_CONCAT2(X, Y) TRACE_CONCAT(X, Y)
+#define ARDUINOTRACE_STRINGIFY(X) #X
+#define ARDUINOTRACE_STRINGIFY2(X) ARDUINOTRACE_STRINGIFY(X)
+#define ARDUINOTRACE_CONCAT(X, Y) X##Y
+#define ARDUINOTRACE_CONCAT2(X, Y) ARDUINOTRACE_CONCAT(X, Y)
 
-#define TRACE_ADD_TRACER(id)                                                   \
-  static const char TRACE_CONCAT(__location, id)[] PROGMEM =                   \
-      __FILE__ ":" TRACE_STRINGIFY(__LINE__) ": ";                             \
-  ArduinoTrace::Tracer TRACE_CONCAT(__tracer, id)(                             \
-      TRACE_CONCAT(__location, id), __PRETTY_FUNCTION__);
+#if ARDUINOTRACE_ENABLE_PROGMEM
+#define ARDUINOTRACE_FLASHIFY(X) F(X)
+#else
+#define ARDUINOTRACE_FLASHIFY(X) X
+#endif
+
+#define ARDUINOTRACE_ADD_TRACER(id)                                            \
+  ArduinoTrace::Tracer ARDUINOTRACE_CONCAT(__tracer, id)(                      \
+      ARDUINOTRACE_FLASHIFY(__FILE__                                           \
+                            ":" ARDUINOTRACE_STRINGIFY2(__LINE__) ": "),       \
+      __PRETTY_FUNCTION__);
+
+#define ARDUINOTRACE_ADD_INIITIALIZER(id, bauds)                               \
+  ArduinoTrace::Initializer ARDUINOTRACE_CONCAT(__initializer, id)(bauds);
 
 // Initializes the Serial port
 //
 // Use this macro only if you want to call TRACE() at global scope,
 // in other cases, call Serial.begin() in your setup() function, as usual.
-#define TRACE_INIT(bauds)                                                      \
-  ArduinoTrace::Initializer TRACE_CONCAT2(__initializer, __COUNTER__)(bauds);
+#define TRACE_INIT(bauds) ARDUINOTRACE_ADD_INIITIALIZER(__COUNTER__, bauds);
 
 // Adds a trace in the Serial port
 //
 // Call this macro anywhere, including at global scope.
 // However, if you use it at global scope, you need to call TRACE_INIT() first,
 // otherwise, the Serial port will not be ready.
-#define TRACE() TRACE_ADD_TRACER(__COUNTER__)
+#define TRACE() ARDUINOTRACE_ADD_TRACER(__COUNTER__)
