bee1b64d287b8c63325a3f22bb7e710f6d0772ca
Benoit Blanchon <github@benoitblanchon.fr>
2019-03-30 12:55:58 +0100

    Reduced the program size by storing each file name only once

--- ArduinoTrace.h
+++ ArduinoTrace.h
@@ -24,7 +24,7 @@
 #endif
 
 #ifndef ARDUINOTRACE_ENABLE_PROGMEM
-#if ARDUINO_ARCH_ESP8266
+#if !defined(ARDUINO) || ARDUINO_ARCH_ESP8266
 // avoid error 'XXX causes a section type conflict with YYY'
 #define ARDUINOTRACE_ENABLE_PROGMEM 0
 #else
@@ -39,23 +39,60 @@ typedef const __FlashStringHelper *prefix_type;
 typedef const char *prefix_type;
 #endif
 
+constexpr unsigned strlen(const char *str) {
+  return str[0] ? strlen(str + 1) + 1 : 0;
+}
+
+template <char... chars>
+struct string {
+#if ARDUINOTRACE_ENABLE_PROGMEM
+  const __FlashStringHelper *data() {
+    static const char buffer[] PROGMEM = {chars...};
+    return reinterpret_cast<const __FlashStringHelper *>(buffer);
+  }
+#else
+  const char *data() {
+    static const char buffer[] = {chars...};
+    return buffer;
+  }
+#endif
+};
+
+template <typename TSourceString, unsigned length, char... chars>
+struct string_maker {
+  using result = typename string_maker<TSourceString, length - 1,
+                                       TSourceString::data()[length - 1],
+                                       chars...>::result;
+};
+
+template <typename TSourceString, char... chars>
+struct string_maker<TSourceString, 0, chars...> {
+  using result = string<chars..., '\0'>;
+};
+
+template <typename TStringSource>
+using make_string =
+    typename string_maker<TStringSource, strlen(TStringSource::data())>::result;
+
 struct Initializer {
-  template <typename TSerial> Initializer(TSerial &serial, int bauds) {
+  template <typename TSerial>
+  Initializer(TSerial &serial, int bauds) {
     serial.begin(bauds);
-    while (!serial)
-      continue;
+    while (!serial) continue;
   }
 };
 
+template <typename TFilename>
 struct Printer {
   template <typename TSerial, typename TValue>
   Printer(TSerial &serial, prefix_type prefix, const TValue &content) {
+    serial.print(make_string<TFilename>{}.data());
     serial.print(prefix);
     serial.println(content);
     serial.flush();
   }
 };
-} // namespace ArduinoTrace
+}  // namespace ArduinoTrace
 
 #define ARDUINOTRACE_STRINGIFY(X) #X
 #define ARDUINOTRACE_CONCAT(X, Y) X##Y
@@ -66,19 +103,23 @@ struct Printer {
 #define ARDUINOTRACE_FLASHIFY(X) X
 #endif
 
-#define ARDUINOTRACE_PRINT(id, prefix, content)                                \
-  ArduinoTrace::Printer ARDUINOTRACE_CONCAT(__tracer, id)(                     \
-      ARDUINOTRACE_SERIAL, ARDUINOTRACE_FLASHIFY(prefix), content);
+#define ARDUINOTRACE_PRINT(id, file, prefix, content)                 \
+  {                                                                   \
+    struct __filename {                                               \
+      constexpr static char const *data() { return file; }            \
+    };                                                                \
+    ArduinoTrace::Printer<__filename> __tracer(                       \
+        ARDUINOTRACE_SERIAL, ARDUINOTRACE_FLASHIFY(prefix), content); \
+  }
 
-#define ARDUINOTRACE_INIITIALIZE(id, bauds)                                    \
-  ArduinoTrace::Initializer ARDUINOTRACE_CONCAT(__initializer, id)(            \
+#define ARDUINOTRACE_INIITIALIZE(id, bauds)                         \
+  ArduinoTrace::Initializer ARDUINOTRACE_CONCAT(__initializer, id)( \
       ARDUINOTRACE_SERIAL, bauds);
 
-#define ARDUINOTRACE_TRACE_PREFIX(file, line)                                  \
-  file ":" ARDUINOTRACE_STRINGIFY(line) ": "
+#define ARDUINOTRACE_TRACE_PREFIX(line) ":" ARDUINOTRACE_STRINGIFY(line) ": "
 
-#define ARDUINOTRACE_DUMP_PREFIX(file, line, variable)                         \
-  file ":" ARDUINOTRACE_STRINGIFY(line) ": " #variable " = "
+#define ARDUINOTRACE_DUMP_PREFIX(line, variable) \
+  ":" ARDUINOTRACE_STRINGIFY(line) ": " #variable " = "
 
 // Initializes the Serial port
 //
@@ -91,17 +132,15 @@ struct Printer {
 // Call this macro anywhere, including at global scope.
 // However, if you use it at global scope, you need to call ARDUINOTRACE_INIT()
 // first, otherwise, the Serial port will not be ready.
-#define TRACE()                                                                \
-  ARDUINOTRACE_PRINT(__COUNTER__,                                              \
-                     ARDUINOTRACE_TRACE_PREFIX(__FILE__, __LINE__),            \
-                     __PRETTY_FUNCTION__)
+#define TRACE()                             \
+  ARDUINOTRACE_PRINT(__COUNTER__, __FILE__, \
+                     ARDUINOTRACE_TRACE_PREFIX(__LINE__), __PRETTY_FUNCTION__)
 
 // Prints the value of a variable.
 //
 // This function will print the name and the value of the variable to the
 // Serial. If you use it at global scope, you need to call ARDUINOTRACE_INIT()
 // first, otherwise, the Serial port will not be ready.
-#define DUMP(variable)                                                         \
-  ARDUINOTRACE_PRINT(__COUNTER__,                                              \
-                     ARDUINOTRACE_DUMP_PREFIX(__FILE__, __LINE__, variable),   \
-                     variable)
+#define DUMP(variable)                      \
+  ARDUINOTRACE_PRINT(__COUNTER__, __FILE__, \
+                     ARDUINOTRACE_DUMP_PREFIX(__LINE__, variable), variable)
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ HEAD
 ----
 
 * Added `ARDUINOTRACE_SERIAL` to allow changing the serial port (issue #1)
+* Reduced the program size by storing each file name only once
 
 1.0.0 (2018/07/19)
 ------------------
